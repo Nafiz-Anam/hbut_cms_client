ff96c9dd1770cf7be0e6f97f485992c9e888edba
Nafiz-Anam <121781159+Nafiz-PureTech@users.noreply.github.com>
2023-05-13 02:21:09 +0600

    changed api url

--- src/apiconstants.js
+++ src/apiconstants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:5000";
-// export const API_BASE_URL = "https://ph-task-api.herokuapp.com";
+// export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL = "https://hbut-course-management-api.onrender.com";
